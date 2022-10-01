74580b225586a1ebfab7ab3c793e2e4be5fe98b0
s123456-max <59247205+s123456-max@users.noreply.github.com>
2022-10-01 19:58:57 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -8,5 +8,8 @@ resp = requests.get('https://i.instagram.com/api/v1/users/web_profile_info/?user
 shortcode = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][1]['node']['shortcode']
 taken_stamp = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][1]['node']['taken_at_timestamp']
 taken_at = datetime.datetime.fromtimestamp(taken_stamp) + datetime.timedelta(hours=8)
+now_at = datetime.datetime.now().timestamp()
 print(f'https://www.instagram.com/p/{shortcode}')
 print(taken_at)
+print(now_at)
+

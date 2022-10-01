f65c055836f520d8eb8c3ba35ae82d14984aa1c9
s123456-max <59247205+s123456-max@users.noreply.github.com>
2022-10-01 20:09:37 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import time
     
 resp = requests.get('https://i.instagram.com/api/v1/users/web_profile_info/?username=alexandrmisko', headers={
     'X-IG-App-ID': '936619743392459',
@@ -8,7 +9,7 @@ resp = requests.get('https://i.instagram.com/api/v1/users/web_profile_info/?user
 shortcode = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][1]['node']['shortcode']
 taken_stamp = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][1]['node']['taken_at_timestamp']
 taken_at = datetime.datetime.fromtimestamp(taken_stamp) + datetime.timedelta(hours=8)
-taken_stamp = datetime.mktime(taken_at)
+taken_stamp = time.mktime(taken_at)
 now_at = int(datetime.datetime.now().timestamp())
 print(f'https://www.instagram.com/p/{shortcode}')
 print(taken_stamp)

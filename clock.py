2366aefe5e541830fceda73d3553b19025df685b
s123456-max <59247205+s123456-max@users.noreply.github.com>
2022-10-02 13:48:12 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -59,14 +59,14 @@ else:
     contents = ''
     media_type = resp.json()['items'][0]['media_type']
     if media_type == 2:
-        contents = f'<button type="button"><a href={resp.json()['items'][0]['video_versions'][0]['url']}>视频</a></button>'
+        contents = '<button type="button"><a href='+resp.json()['items'][0]['video_versions'][0]['url']+'>视频</a></button>'
     else:
         items = resp.json()['items']['carousel_media']
         for item in items:
             if item['media_type'] == 1:
-                contents += f'<button type="button"><a href={item['image_versions2']['candidates'][0]['url']}>图片</a></button>'
+                contents += '<button type="button"><a href='+resp.json()['items'][0]['image_versions2']['candidates'][0]['url']+'>图片</a></button>'
             else:
-                contents += f'<button type="button"><a href={item['video_versions'][0]['url']}>视频</a></button>'
+                contents += '<button type="button"><a href='+resp.json()['items'][0]['video_versions'][0]['url']+'>视频</a></button>'
     yag.send(to='1586924294@qq.com', subject='AlexandrMisko更新啦！', contents='<h1>方式1（动态--复制下面链接到有Instagram登录状态的浏览器中打开）：</h1>'+url+'<h1>方式2（视频--直接打开即可）：</h1>'+contents)
     yag.close()
     print('发送邮件成功！')

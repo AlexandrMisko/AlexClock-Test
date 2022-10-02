4f976de3dcd5f91ed27e151aa87c7944c9820e73
s123456-max <59247205+s123456-max@users.noreply.github.com>
2022-10-02 14:45:13 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -9,18 +9,18 @@ resp = requests.get('https://i.instagram.com/api/v1/users/web_profile_info/?user
     'Cookie': 'csrftoken=UcTWfv2NroJABRj4dIWfe38eRpb1CXU1; mid=YZIUAwALAAHIMaHw27iWm4uRNhXm; ig_did=267B8A84-7E4C-4AE8-8310-89537C6AE002; ig_nrcb=1; ds_user_id=48282355544; sessionid=48282355544%3AuWW0eZDfR8CL3u%3A27; datr=ZWq4YgmzX9ZT1JWqg_JKOBhe; dpr=1.25; shbid="12194\05448282355544\0541688611539:01f75d3a60c811bfcd880e4e8f3baefa76a97af80c8452eb788f385d096964b8d7380896"; shbts="1657075539\05448282355544\0541688611539:01f790a0918bea636a217097bd7087021d8198bf00f019bb8c87e02a0a702e02d87c1d7e"; rur="NAO\05448282355544\0541688617994:01f7c149d91540dacf8374476beca35130e516943a12ed01f4c487859acfb14033ca7907'
 })
 taken_stamp = ['']*4
-taken_stamp[0] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][3]['node']['taken_at_timestamp']
-taken_stamp[1] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][4]['node']['taken_at_timestamp']
-taken_stamp[2] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][5]['node']['taken_at_timestamp']
-taken_stamp[3] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][6]['node']['taken_at_timestamp']
+taken_stamp[0] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['taken_at_timestamp']
+taken_stamp[1] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][1]['node']['taken_at_timestamp']
+taken_stamp[2] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][2]['node']['taken_at_timestamp']
+taken_stamp[3] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][3]['node']['taken_at_timestamp']
 rank = 0
 for i in range(1,4):
     if taken_stamp[0] < taken_stamp[i]:
         taken_stamp[0] = taken_stamp[i]
         rank = i
 taken_at = (datetime.datetime.fromtimestamp(taken_stamp[rank]) + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
-id = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][rank+3]['node']['id']
-shortcode = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][rank+3]['node']['shortcode']
+id = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][rank]['node']['id']
+shortcode = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][rank]['node']['shortcode']
 # now_at = (datetime.datetime.now() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
 # print(now_at)
 url = f'https://www.instagram.com/p/{shortcode}'

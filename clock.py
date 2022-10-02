33a3b14d4102f93be8b901ccbe5f9c54915c7090
s123456-max <59247205+s123456-max@users.noreply.github.com>
2022-10-02 10:35:33 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -9,6 +9,7 @@ resp = requests.get('https://i.instagram.com/api/v1/users/web_profile_info/?user
     'Cookie': 'csrftoken=UcTWfv2NroJABRj4dIWfe38eRpb1CXU1; mid=YZIUAwALAAHIMaHw27iWm4uRNhXm; ig_did=267B8A84-7E4C-4AE8-8310-89537C6AE002; ig_nrcb=1; ds_user_id=48282355544; sessionid=48282355544%3AuWW0eZDfR8CL3u%3A27; datr=ZWq4YgmzX9ZT1JWqg_JKOBhe; dpr=1.25; shbid="12194\05448282355544\0541688611539:01f75d3a60c811bfcd880e4e8f3baefa76a97af80c8452eb788f385d096964b8d7380896"; shbts="1657075539\05448282355544\0541688611539:01f790a0918bea636a217097bd7087021d8198bf00f019bb8c87e02a0a702e02d87c1d7e"; rur="NAO\05448282355544\0541688617994:01f7c149d91540dacf8374476beca35130e516943a12ed01f4c487859acfb14033ca7907'
 })
 id = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][1]['node']['id']
+shortcode = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][1]['node']['shortcode']
 taken_stamp = ['']*4
 taken_stamp[0] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['taken_at_timestamp']
 taken_stamp[1] = resp.json()['data']['user']['edge_owner_to_timeline_media']['edges'][1]['node']['taken_at_timestamp']
@@ -47,7 +48,7 @@ else:
         'Cookie': 'csrftoken=UcTWfv2NroJABRj4dIWfe38eRpb1CXU1; mid=YZIUAwALAAHIMaHw27iWm4uRNhXm; ig_did=267B8A84-7E4C-4AE8-8310-89537C6AE002; ig_nrcb=1; ds_user_id=48282355544; sessionid=48282355544%3AuWW0eZDfR8CL3u%3A27%3AAYdStGPAuKILQTiONRS6ue7nELp1L-iY5ST6OKlEhJo; datr=ZWq4YgmzX9ZT1JWqg_JKOBhe; dpr=1.25; shbid="12194\05448282355544\0541696081072:01f7d69ed0200ec38ee14a29d422334c1169d5d5169af9af61b29e2de4953b3be7303174"; shbts="1664545072\05448282355544\0541696081072:01f74d2063e970bc6f4b80a7a089c85acd30a459d48c285bf13282f13c9cb5e1a2bcddb8"; rur="NAO\05448282355544\0541696165910:01f7b9d8a44beaeeebd30b27f51a1bde5163a188b1d2da74c7d94b0e2a61223d51e79078'
     })
     print(resp.text)
-    resp = requests.get('https://i.instagram.com/api/v1/media/2937327646954046117/info/', headers={
+    resp = requests.get(f'https://i.instagram.com/api/v1/media/{id}/info/', headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0',
         'X-IG-App-ID': '936619743392459',
         'Cookie': 'csrftoken=UcTWfv2NroJABRj4dIWfe38eRpb1CXU1; mid=YZIUAwALAAHIMaHw27iWm4uRNhXm; ig_did=267B8A84-7E4C-4AE8-8310-89537C6AE002; ig_nrcb=1; ds_user_id=48282355544; sessionid=48282355544%3AuWW0eZDfR8CL3u%3A27%3AAYdStGPAuKILQTiONRS6ue7nELp1L-iY5ST6OKlEhJo; datr=ZWq4YgmzX9ZT1JWqg_JKOBhe; dpr=1.25; shbid="12194\05448282355544\0541696081072:01f7d69ed0200ec38ee14a29d422334c1169d5d5169af9af61b29e2de4953b3be7303174"; shbts="1664545072\05448282355544\0541696081072:01f74d2063e970bc6f4b80a7a089c85acd30a459d48c285bf13282f13c9cb5e1a2bcddb8"; rur="NAO\05448282355544\0541696165910:01f7b9d8a44beaeeebd30b27f51a1bde5163a188b1d2da74c7d94b0e2a61223d51e79078'
